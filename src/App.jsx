1e5e6ac34c7d30049bffcc9e6d1233dfbe8ae3d2
johnkennady220 <johnkennady220@gmail.com>
2023-10-08 12:40:51 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Signup from './Signup'
+import {BrowserRouter,Routes, Route} from 'react-router-dom'
+import Login from './Login'
+import Home from './Home'
+import ForgotPassword from './ForgotPassword'
+import ResetPassword from './reset_password'
+function App() {
+
+  return (
+   <BrowserRouter>
+   <Routes>
+    <Route path="/register" element={<Signup/>}></Route>
+    <Route path="/login" element={<Login/>}></Route>
+    <Route path="/home" element={<Home/>}></Route> 
+    <Route path="/forgot-password" element={<ForgotPassword />}></Route>
+    <Route path="/reset_password/:id/:token" element={<ResetPassword />}></Route>  
+   </Routes>
+   </BrowserRouter>
+  )
+}
+
+export default App
